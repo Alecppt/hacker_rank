da95db1e559da91f775068a8eedc599bb122d5e9
Alec Chen <goodneighboralec@gmail.com>
2018-01-30 15:37:38 -0800

    finished delete node at index

---
+++ data_structure/Linked_Lists/Delete_Node_At_Index.cpp
@@ -0,0 +1,45 @@
+/*
+  Delete Node at a given position in a linked list 
+  Node is defined as 
+  struct Node
+  {
+     int data;
+     struct Node *next;
+  }
+*/
+Node* Delete(Node *head, int position)
+{
+  Node *tmp;
+  if (head == nullptr)
+  {
+      return head;
+  }
+  if( position == 0)
+  {
+      tmp = head->next;
+      delete head;
+      head = tmp;
+  }
+  else
+  {
+    Node *current = head;
+    int i = 0;
+    while(i!= position -1 && current->next != nullptr)
+    {
+      current = current->next;
+      i++;
+    }
+    if(current->next == nullptr)
+    {
+       //at the end of index, maybe the position given exceeds the total number of nodes
+       // nothing to delete 
+    }
+    else
+    {   tmp = current->next->next;
+        delete current->next;
+        current->next = tmp;
+    }
+  }
+  
+    return head;
+}
